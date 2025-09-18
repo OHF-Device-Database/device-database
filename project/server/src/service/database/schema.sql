8269bb978e72c86ed79986bfd1f0ef506cd92668
Philip Trauner <philip.trauner@arztpraxis.io>
2025-09-18 12:16:34 +0200

    fix: schema / migration diverge

--- project/server/src/service/database/schema.sql
+++ project/server/src/service/database/schema.sql
@@ -1,6 +1,6 @@
 create table snapshot (
     id text not null primary key,
-    version integer not null,
+    version integer not null default -1,
     data text not null,
     contact text not null,
     created_at integer not null
