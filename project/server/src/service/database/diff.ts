7677bc23c8c201c827877ba190a7701c82dad5c4
Philip Trauner <philip.trauner@arztpraxis.io>
2025-09-18 12:23:58 +0200

    tooling: script that detects schema / migration divergence

---
+++ project/server/src/service/database/diff.ts
@@ -0,0 +1,349 @@
+import { readFile } from "node:fs/promises";
+import { parseArgs } from "node:util";
+
+import { isSome } from "../../type/maybe";
+import { unroll } from "../../utility/iterable";
+import { Database } from "./";
+import { DatabaseMigrate, MIGRATION_TABLE_NAME } from "./migrate";
+
+const options = {
+	schema: {
+		type: "string",
+	},
+	"migration-directory": {
+		type: "string",
+	},
+} as const;
+
+const { values } = parseArgs({ options });
+
+if (typeof values.schema === "undefined") {
+	console.error(
+		"required parameter '--schema' missing (location of database schema)",
+	);
+	process.exit(1);
+}
+if (typeof values["migration-directory"] === "undefined") {
+	console.error(
+		"required parameter '--migration-directory' missing (location of migration directory)",
+	);
+	process.exit(1);
+}
+
+// holds schema definition
+const a = new Database(":memory:", false);
+// holds migrations
+const b = new Database(":memory:", false);
+
+const migrate = new DatabaseMigrate(b);
+const migrations = await unroll(
+	DatabaseMigrate.migrations(values["migration-directory"]),
+);
+const plan = await migrate.plan(migrations);
+if (!DatabaseMigrate.viable(plan)) {
+	console.error("migration plan not viable");
+	console.error(plan);
+	process.exit(1);
+}
+
+await a.exec(await readFile(values.schema, { encoding: "utf-8" }));
+await migrate.act(plan);
+
+let failed = false;
+
+const PREFIXES = ["table", "view", "trigger", "index"] as const;
+type Prefix = typeof PREFIXES[number];
+
+const LONGEST_PREFIX_LENGTH: number = PREFIXES.reduce((acc: number, item: string) => {
+  const length = item.length;
+  return length > acc ? length : acc;
+}, 0);
+
+const prefix = (prefix: Prefix) => {
+  return `${prefix.padStart(LONGEST_PREFIX_LENGTH, " ")} |`
+}
+
+{
+	const query = `
+	  select
+  	  sm.name "table",
+  		p.name "column",
+  		p.type,
+  		p."notnull" "notNull",
+  		p.dflt_value "defaultValue",
+  		p.pk "primaryKey"
+		from
+		  sqlite_master sm, pragma_table_info(sm.name) p
+		where
+		  sm.type = 'table' and sm.name != '${MIGRATION_TABLE_NAME}';
+	`;
+
+	type QualifiedColumn = {
+		table: string;
+		column: string;
+		type: string;
+		notNull: number;
+		defaultValue: string | null;
+		primaryKey: number;
+	};
+
+	type UnqualifiedColumn = Omit<QualifiedColumn, "table" | "column">;
+
+	const aSchema: Map<
+		// table name
+		string,
+		Map<
+			// column name
+			string,
+			UnqualifiedColumn
+		>
+	> = new Map();
+	for await (const row of a.query(query, { returnArray: false }, {})) {
+		const cast = row as QualifiedColumn;
+
+		const bucket = aSchema.get(cast.table);
+		if (typeof bucket === "undefined") {
+			aSchema.set(cast.table, new Map([[cast.column, cast]]));
+		} else {
+			bucket.set(cast.column, cast);
+		}
+	}
+
+	const bSchema: Map<
+		// table name
+		string,
+		Map<
+			// column name
+			string,
+			UnqualifiedColumn
+		>
+	> = new Map();
+	for await (const row of b.query(query, { returnArray: false }, {})) {
+		const cast = row as QualifiedColumn;
+
+		const bucket = bSchema.get(cast.table);
+		if (typeof bucket === "undefined") {
+			bSchema.set(cast.table, new Map([[cast.column, cast]]));
+		} else {
+			bucket.set(cast.column, cast);
+		}
+	}
+
+	for (const [table, aColumns] of aSchema) {
+		const bColumns = bSchema.get(table);
+		if (typeof bColumns === "undefined") {
+			console.error(`${prefix("table")} <${table}> missing in migrations`);
+			failed = true;
+			continue;
+		}
+
+		for (const [aColumnName, aColumn] of aColumns) {
+			const bColumn = bColumns.get(aColumnName);
+			if (typeof bColumn === "undefined") {
+				console.error(`${prefix("table")} [${table}.${aColumnName}] is missing in migrations`);
+				failed = true;
+				continue;
+			}
+
+			if (aColumn.type !== bColumn.type) {
+				console.error(
+					`${prefix("table")} [${table}.${aColumnName}] has type <${aColumn.type}> in schema, <${bColumn.type}> in migrations`,
+				);
+				failed = true;
+			}
+
+			if (aColumn.notNull !== bColumn.notNull) {
+				console.error(
+					`${prefix("table")} [${table}.${aColumnName}] is ${aColumn.notNull ? "<not null>" : "<null>"} in schema, but is ${bColumn.notNull ? "<not null>" : "<null>"} in migrations`,
+				);
+				failed = true;
+			}
+
+			if (aColumn.defaultValue !== bColumn.defaultValue) {
+				console.error(
+					`${prefix("table")} [${table}.${aColumnName}] has ${isSome(aColumn.defaultValue) ? `default value <${aColumn.defaultValue}>` : "no default value"} in schema, but ${isSome(bColumn.defaultValue) ? `<${bColumn.defaultValue}>` : "no default value"} in migrations`,
+				);
+				failed = true;
+			}
+
+			if (aColumn.primaryKey !== bColumn.primaryKey) {
+				console.error(
+					`${prefix("table")} [${table}.${aColumnName}] is ${aColumn.primaryKey ? "a primary key" : "not a primary key"} in schema, but is ${bColumn.primaryKey ? "a primary key" : "not a primary key"} in migrations`,
+				);
+				failed = true;
+			}
+		}
+	}
+}
+
+{
+	const query = `
+    select
+      name,
+      sql
+    from
+      sqlite_master
+    where
+      type = 'view'
+	`;
+
+	type Row = {
+		name: string;
+		sql: string;
+	};
+
+	const aSchema: Map<
+		// view name
+		string,
+		// view definition
+		string
+	> = new Map();
+	for await (const row of a.query(query, { returnArray: false }, {})) {
+		const cast = row as Row;
+		aSchema.set(cast.name, cast.sql);
+	}
+
+	const bSchema: Map<
+		// view name
+		string,
+		// view definition
+		string
+	> = new Map();
+	for await (const row of b.query(query, { returnArray: false }, {})) {
+		const cast = row as Row;
+		bSchema.set(cast.name, cast.sql);
+	}
+
+	for (const [aName, aDefinition] of aSchema) {
+		const bDefinition = bSchema.get(aName);
+		if (typeof bDefinition === "undefined") {
+			console.warn(`${prefix("view")} <${aName}> missing in migrations`);
+			failed = true;
+			continue;
+		}
+
+		if (aDefinition !== bDefinition) {
+			console.warn(`${prefix("view")} <${aName}> definiton mismatch`);
+			console.log(">>> schema")
+			console.log((aDefinition));
+			console.log(">>> migration")
+      console.log((bDefinition));
+		}
+	}
+}
+
+{
+	const query = `
+	  select
+			name,
+			sql
+		from
+		  sqlite_master
+		where
+		  type = 'index' and
+			-- filter out automatically created indexes
+			sql != '';
+	`;
+
+	type Row = {
+		name: string;
+		sql: string;
+	};
+
+	const aSchema: Map<
+		// index name
+		string,
+		// index definition
+		string
+	> = new Map();
+	for await (const row of a.query(query, { returnArray: false }, {})) {
+		const cast = row as Row;
+		aSchema.set(cast.name, cast.sql);
+	}
+
+	const bSchema: Map<
+		// index name
+		string,
+		// index definition
+		string
+	> = new Map();
+	for await (const row of b.query(query, { returnArray: false }, {})) {
+		const cast = row as Row;
+		bSchema.set(cast.name, cast.sql);
+	}
+
+	for (const [aName, aDefinition] of aSchema) {
+		const bDefinition = bSchema.get(aName);
+		if (typeof bDefinition === "undefined") {
+			console.warn(`${prefix("index")} <${aName}> missing in migrations`);
+			failed = true;
+			continue;
+		}
+
+		if (aDefinition !== bDefinition) {
+			console.warn(`${prefix("index")} <${aName}> definiton mismatch`);
+			console.log(">>> schema")
+			console.log((aDefinition));
+			console.log(">>> migration")
+      console.log((bDefinition));
+		}
+	}
+}
+
+{
+	const query = `
+    select
+      name,
+      sql
+    from
+      sqlite_master
+    where
+      type = 'trigger';
+  `;
+
+	type Row = {
+		name: string;
+		sql: string;
+	};
+
+	const aSchema: Map<
+		// trigger name
+		string,
+		// trigger definition
+		string
+	> = new Map();
+	for await (const row of a.query(query, { returnArray: false }, {})) {
+		const cast = row as Row;
+		aSchema.set(cast.name, cast.sql);
+	}
+
+	const bSchema: Map<
+		// trigger name
+		string,
+		// trigger definition
+		string
+	> = new Map();
+	for await (const row of b.query(query, { returnArray: false }, {})) {
+		const cast = row as Row;
+		bSchema.set(cast.name, cast.sql);
+	}
+
+	for (const [aName, aDefinition] of aSchema) {
+		const bDefinition = bSchema.get(aName);
+		if (typeof bDefinition === "undefined") {
+			console.warn(`${prefix("trigger")} <${aName}> missing in migrations`);
+			failed = true;
+			continue;
+		}
+
+		if (aDefinition !== bDefinition) {
+			console.warn(`${prefix("trigger")} <${aName}> definiton mismatch`);
+			console.log(">>> schema")
+			console.log((aDefinition));
+			console.log(">>> migration")
+      console.log((bDefinition));
+		}
+	}
+}
+
+process.exit(failed ? 1 : 0);
