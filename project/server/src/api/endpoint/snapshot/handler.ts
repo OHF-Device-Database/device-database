1e153357b3493c6ba1e043092bca3ced25d52303
Philip Trauner <philip.trauner@arztpraxis.io>
2025-09-22 10:33:49 +0200

    feat: improve external email address error

--- project/server/src/api/endpoint/snapshot/handler.ts
+++ project/server/src/api/endpoint/snapshot/handler.ts
@@ -1,16 +1,50 @@
-import { SnapshotImportSnapshot } from "../../../service/snapshot";
+import { Schema } from "effect/index";
+
+import {
+	SnapshotContact,
+	type SnapshotImportSnapshot,
+} from "../../../service/snapshot";
 import { effectfulEndpoint, NoParameters } from "../../base";
 
 import type { Dependency } from "../../dependency";
+
+const RequestBody = Schema.Struct({
+	contact: Schema.String,
+	data: Schema.Unknown,
+});
+
+const domains = ["openhomefoundation.org", "nabucasa.com"] as const;
+type Assert = `${string}${(typeof domains)[number]}` extends SnapshotContact
+	? "yes"
+	: "no";
+const _: Assert = "yes";
 
 export const postSnapshot = (d: Pick<Dependency, "snapshot">) =>
 	effectfulEndpoint(
 		"/api/v1/snapshot",
 		"post",
 		NoParameters,
-		SnapshotImportSnapshot,
+		RequestBody,
 		async (_, requestBody) => {
-			await d.snapshot.import(requestBody);
+			let validated: SnapshotImportSnapshot;
+			{
+				const contact = requestBody.contact;
+
+				const guard = Schema.is(SnapshotContact);
+				if (guard(contact)) {
+					validated = {
+						contact,
+						data: requestBody.data,
+					};
+				} else {
+					return {
+						code: 400,
+						body: `sorry, right now we only accept submissions from email addresses ending with: ${domains.join(", ")}`,
+					} as const;
+				}
+			}
+
+			await d.snapshot.import(validated);
 
 			return {
 				code: 200,
