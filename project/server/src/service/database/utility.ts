562d7496dcd73c72481cd2ee91c85e73ca2c0084
Philip Trauner <philip.trauner@arztpraxis.io>
2025-09-27 11:04:14 +0200

    chore: utility to bring up database with all migrations applied for testing

---
+++ project/server/src/service/database/utility.ts
@@ -0,0 +1,30 @@
+import { join } from "node:path";
+
+import { unroll } from "../../utility/iterable";
+import { Database } from ".";
+import { DatabaseMigrate } from "./migrate";
+
+const __dirname = import.meta.dirname;
+
+/* c8 ignore start */
+export const testDatabase = async (): Promise<Database> => {
+	const database = new Database(":memory:", false);
+	{
+		const migrate = new DatabaseMigrate(database);
+		const migrations = await unroll(
+			DatabaseMigrate.migrations(
+				join(__dirname, "..", "database", "migration"),
+			),
+		);
+
+		const plan = await migrate.plan(migrations);
+		if (!DatabaseMigrate.viable(plan)) {
+			throw new Error("unreachable");
+		}
+
+		await migrate.act(plan);
+	}
+
+	return database;
+};
+/* c8 ignore stop */
