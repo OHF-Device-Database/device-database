OHF-Device-Database/device-database

commit 1b9602f935b833e083a52a74cde1f2362ef7c81c

submission endpoint (#24)
* fix: fully consume stdin, not just first <n> bytes

protocol buffers where only comsumed partially, resulting in decoding errors

* feat: support "Delete" queries

* chore: schema definition for submission endpoint

* chore: switch to `sqlc` fork

* chore: remove outdated disclaimer

* chore: document switch to node.js's test runner

* feat: snapshot submission endpoint

* feat: validate individually and print decoding errors

project/server/src/api/base.ts
@@ -5,6 +5,7 @@ import { isLeft } from "effect/Either";
 import { Hono } from "hono";
 import type { StatusCode } from "hono/utils/http-status";
 
+import { isNone } from "../type/maybe";
 import { RequestStorage, requestStorage } from "../utility/request-storage";
 
 import type { paths } from "../schema";
@@ -23,8 +24,10 @@ type RequestBodyContentType =
 	| "application/json"
 	| "application/x-www-form-urlencoded";
 
-export const NoParameters = Schema.Struct({});
-export const NoRequestBody = Schema.Struct({});
+export const NoParameters = Symbol("NoParameters");
+type NoParameters = typeof NoParameters;
+export const NoRequestBody = Symbol("NoRequestBody");
+type NoRequestBody = typeof NoRequestBody;
 
 type EndpointRequestParameters<
 	Path extends keyof paths,
@@ -33,12 +36,20 @@ type EndpointRequestParameters<
 	? paths[Path][Method]["parameters"] extends Record<string, never>
 		? { parameters?: paths[Path][Method]["parameters"] }
 		: // only support parameter types that extend string
-			// additionally enforced by schema linter enforces
+			// additionally enforced by schema linter
 			{
 				parameters: {
 					[Type in keyof paths[Path][Method]["parameters"]]: {
-						[Parameter in keyof paths[Path][Method]["parameters"][Type]]: paths[Path][Method]["parameters"][Type][Parameter] extends string
-							? paths[Path][Method]["parameters"][Type][Parameter]
+						[Parameter in keyof Required<
+							paths[Path][Method]["parameters"]
+						>[Type]]: Required<
+							Required<paths[Path][Method]["parameters"]>[Type]
+						>[Parameter] extends string | undefined
+							?
+									| Required<
+											Required<paths[Path][Method]["parameters"]>[Type]
+									  >[Parameter]
+									| undefined
 							: never;
 					};
 				};
@@ -66,7 +77,6 @@ type EndpointRequest<
 	Method extends keyof paths[Path],
 > = EndpointRequestParameters<Path, Method> &
 	EndpointRequestRequestBody<Path, Method>;
-
 type EndpointResponses<
 	Path extends keyof paths,
 	Method extends keyof paths[Path],
@@ -101,12 +111,12 @@ export type EndpointResponse<
 // OpenAPI spec can specify arbitrary types for parameters, but parsing will always yield strings
 type StringParameters<P> = {
 	[T in keyof P]: {
-		[N in keyof P[T]]: string;
+		[N in keyof P[T]]?: string | undefined;
 	};
 };
 
 export type DecoratedHandler<H> = {
-	for: {
+	for?: {
 		path: string;
 		method: string;
 		handler: H;
@@ -134,17 +144,19 @@ export const idempotentEndpoint = <
 		? EndpointRequest<Path, Method>["parameters"]
 		: never,
 	Code extends keyof EndpointResponses<Path, Method>,
-	P extends Schema.Any,
+	P extends Schema.Schema.AnyNoContext,
 >(
 	path: Path,
 	method: Method,
-	parameters: Record<string, undefined> extends Parameters
-		? typeof NoParameters
+	parameters: Record<string, never> | undefined extends Parameters
+		? NoParameters
 		: Schema.Schema.Encoded<P> extends StringParameters<Parameters>
 			? P
 			: never,
 	handler: (
-		parameters: Schema.Schema.Type<P>,
+		parameters_: typeof parameters extends NoParameters
+			? never
+			: Schema.Schema.Type<P>,
 	) => Promise<
 		Code extends number ? EndpointResponse<Path, Method, Code> : never
 	>,
@@ -164,10 +176,9 @@ export const idempotentEndpoint = <
 			),
 		};
 
-		// biome-ignore lint/suspicious/noExplicitAny: inferred `Any` / `Schema<{}>` contradict each other
-		const decodedParameters = Schema.decodeUnknownEither(parameters as any)(
-			receivedParameters,
-		);
+		const decodedParameters = Schema.decodeUnknownEither(
+			parameters as Schema.Schema.AnyNoContext,
+		)(receivedParameters);
 		if (isLeft(decodedParameters)) {
 			return c.text(decodedParameters.left.message, 400);
 		}
@@ -227,6 +238,10 @@ export const effectfulEndpoint = <
 	ContentType extends "kind" extends keyof RequestBody
 		? RequestBody["kind"]
 		: never,
+	ConcreteRequestBody extends Extract<
+		RequestBody,
+		{ kind: ContentType }
+	>["body"],
 	Code extends keyof EndpointResponses<Path, Method>,
 	Contextualize extends EffectfulEndpointContext,
 	Context extends {
@@ -236,31 +251,29 @@ export const effectfulEndpoint = <
 				: undefined;
 		};
 	},
-	P extends Schema.Any,
-	RB extends Schema.Any,
+	P extends Schema.Schema.AnyNoContext,
+	RB extends Schema.Schema.AnyNoContext,
 >(
 	path: Path,
 	method: Method,
-	parameters: Record<string, undefined> extends Parameters
-		? typeof NoParameters
+	parameters: Record<string, never> | undefined extends Parameters
+		? NoParameters
 		: Schema.Schema.Encoded<P> extends StringParameters<Parameters>
 			? P
 			: never,
 	contentType: ContentType extends RequestBodyContentType ? ContentType : never,
-	requestBody: Record<string, undefined> extends Extract<
-		RequestBody,
-		{ kind: ContentType }
-	>["body"]
-		? typeof NoRequestBody
-		: Schema.Schema.Encoded<RB> extends Extract<
-					RequestBody,
-					{ kind: ContentType }
-				>["body"]
+	requestBody: Record<string, undefined> extends ConcreteRequestBody
+		? NoRequestBody
+		: Schema.Schema.Encoded<RB> extends ConcreteRequestBody
 			? RB
 			: never,
 	handler: (
-		parameters: Schema.Schema.Type<P>,
-		requestBody: Schema.Schema.Type<RB>,
+		parameters_: typeof parameters extends NoParameters
+			? never
+			: Schema.Schema.Type<P>,
+		requestBody_: typeof requestBody extends NoRequestBody
+			? never
+			: Schema.Schema.Type<RB>,
 		context: Context,
 	) => Promise<
 		Code extends number ? EndpointResponse<Path, Method, Code> : never
@@ -330,18 +343,16 @@ export const effectfulEndpoint = <
 			return c.text("malformed request body", 400);
 		}
 
-		// biome-ignore lint/suspicious/noExplicitAny: inferred `Any` / `Schema<{}>` contradict each other
-		const decodedParameters = Schema.decodeUnknownEither(parameters as any)(
-			receivedParameters,
-		);
+		const decodedParameters = Schema.decodeUnknownEither(
+			parameters as Schema.Schema.AnyNoContext,
+		)(receivedParameters);
 		if (isLeft(decodedParameters)) {
 			return c.text(decodedParameters.left.message, 400);
 		}
 
-		// biome-ignore lint/suspicious/noExplicitAny: inferred `Any` / `Schema<{}>` contradict each other
-		const decodedRequestBody = Schema.decodeUnknownEither(requestBody as any)(
-			receivedRequestBody,
-		);
+		const decodedRequestBody = Schema.decodeUnknownEither(
+			requestBody as Schema.Schema.AnyNoContext,
+		)(receivedRequestBody);
 		if (isLeft(decodedRequestBody)) {
 			return c.text(decodedRequestBody.left.message, 400);
 		}
@@ -387,5 +398,124 @@ export const effectfulEndpoint = <
 	});
 
 	return { router, for: { path, method, handler: handler } };
+};
+/* node:coverage enable */
+
+// TODO: test
+/* node:coverage disable */
+export const effectfulSinkEndpoint = <
+	Path extends keyof paths,
+	Method extends keyof {
+		[Method in keyof Pick<
+			paths[Path],
+			EffectfulHttpMethod
+		> as paths[Path][Method] extends undefined ? never : Method]: never;
+	} &
+		string,
+	Parameters extends EndpointRequest<Path, Method>["parameters"],
+	RequestBody extends EndpointRequest<Path, Method>["requestBody"],
+	ContentType extends "kind" extends keyof RequestBody
+		? RequestBody["kind"]
+		: never,
+	Code extends keyof EndpointResponses<Path, Method>,
+	P extends Schema.Schema.AnyNoContext,
+>(
+	path: Path,
+	method: Method,
+	parameters: Record<string, never> | undefined extends Parameters
+		? NoParameters
+		: Schema.Schema.Encoded<P> extends StringParameters<Parameters>
+			? P
+			: never,
+	contentType: ContentType extends RequestBodyContentType ? ContentType : never,
+	handler: (
+		parameters_: typeof parameters extends NoParameters
+			? never
+			: Schema.Schema.Type<P>,
+		// biome-ignore lint/suspicious/noExplicitAny: handler has to perform validation
+		requestBody_: ReadableStream<any>,
+	) => Promise<
+		Code extends number ? EndpointResponse<Path, Method, Code> : never
+	>,
+): DecoratedHandler<typeof handler> => {
+	const router = new Hono();
+
+	const substitutedPath = path.replaceAll(pathParameter, ":$<parameter>");
+
+	const route = router[method as EffectfulHttpMethod].bind(router);
+
+	route(substitutedPath, async (c) => {
+		const receivedParameters = {
+			query: c.req.query(),
+			path: c.req.param(),
+			header: Object.fromEntries(
+				Object.entries(c.req.header()).map(
+					([key, value]) => [key.toLowerCase(), value] as const,
+				),
+			),
+		};
+
+		const receivedRequestBodyContentType = c.req.header("Content-Type");
+		if (typeof receivedRequestBodyContentType === "undefined") {
+			return c.text("missing content type", 400);
+		}
+
+		// TODO: when multiple handlers are defined on the same route for different
+		// content types, only the first one will fire
+		if (receivedRequestBodyContentType !== contentType) {
+			return c.text("unexpected content type", 400);
+		}
+
+		const requestBody = c.req.raw.body;
+		if (isNone(requestBody)) {
+			return c.text("unexpected empty request body", 400);
+		}
+
+		const decodedParameters = Schema.decodeUnknownEither(
+			parameters as Schema.Schema.AnyNoContext,
+		)(receivedParameters);
+		if (isLeft(decodedParameters)) {
+			return c.text(decodedParameters.left.message, 400);
+		}
+
+		const response = await requestStorage.run(
+			// use store created by middleware and create new store for SSR
+			requestStorage.getStore() ?? new RequestStorage(),
+			async () => {
+				return await handler(
+					// biome-ignore lint/suspicious/noExplicitAny: types are checked above
+					decodedParameters.right as any,
+					requestBody,
+				);
+			},
+		);
+
+		c.status(response.code as StatusCode);
+
+		if ("headers" in response && typeof response.headers !== "undefined") {
+			const headers = response.headers as Record<string, string | string[]>;
+			for (const [key, value] of Object.entries(headers)) {
+				if (typeof value === "undefined") {
+					continue;
+				}
+
+				if (!Array.isArray(value)) {
+					c.header(key, value);
+				} else {
+					for (const item of value) {
+						c.header(key, item);
+					}
+				}
+			}
+		}
+
+		if (typeof response.body === "object") {
+			return c.json(response.body);
+		} else {
+			return c.text(`${response.body}`);
+		}
+	});
+
+	return { router };
 };
 /* node:coverage enable */

.github/workflows/qa-server.yml
@@ -29,7 +29,7 @@ jobs:
         working-directory: project/server
         run: npm ci
       - name: install dependencies (server / sqlc)
-        run: sudo snap install sqlc
+        run: make install-dependency-sqlc
       - name: lint
         working-directory: project/server
         run: make lint
@@ -60,7 +60,7 @@ jobs:
         working-directory: project/server
         run: npm ci
       - name: install dependencies (server / sqlc)
-        run: sudo snap install sqlc
+        run: make install-dependency-sqlc
       - name: test
         working-directory: project/server
         run: make test

.gitignore
@@ -1,3 +1,4 @@
+.ephemeral/
 node_modules/
 .tap
 .DS_Store

Makefile
@@ -1,5 +1,8 @@
-.PHONY: build test build-container
+.PHONY: build test build-container install-dependency-sqlc
 .NOTPARALLEL: build
+
+EPHEMERAL_DIR := .ephemeral
+EPHEMERAL_SQLC_DIR := $(EPHEMERAL_DIR)/sqlc
 
 build:
 	$(MAKE) --directory schema
@@ -11,3 +14,10 @@ test:
 
 build-container:
 	$(MAKE) --directory project/server -f make/ops.mk build-container
+
+$(EPHEMERAL_DIR):
+	mkdir -p $(EPHEMERAL_DIR)
+
+install-dependency-sqlc: | $(EPHEMERAL_DIR)
+	[ -d '$(EPHEMERAL_SQLC_DIR)' ] || git clone https://github.com/OHF-Device-Database/sqlc.git $(EPHEMERAL_SQLC_DIR)
+	cd '$(EPHEMERAL_SQLC_DIR)'; GOPATH='$(abspath .ephemeral/go)' go install ./cmd/sqlc

project/server/make/common.mk
@@ -1,5 +1,7 @@
 .PHONY: build query clean
 
+SQLC_BIN := ../../.ephemeral/go/bin/sqlc
+
 CLIENT_IN := \
 	$(shell find -L package/client/src -type f)
 
@@ -44,7 +46,7 @@ query: $(SERVER_QUERY_OUT)
 # patterns can be used to define multi-output rules → replace least significant character
 # with pattern wildcard symbol
 $(subst .,%,$(SERVER_QUERY_QUERY_OUT)): $(SERVER_QUERY_QUERY_IN)
-	@sqlc generate
+	@$(SQLC_BIN) generate
 
 $(subst .,%,$(SERVER_OUT)): $(SERVER_IN)
 	@npm exec -- tsc --project tsconfig.json --incremental --noEmit

project/server/src/api/dependency.ts
@@ -3,6 +3,7 @@ import type { Hono } from "hono";
 import { container } from "../dependency";
 import { ICallbackVendorSlack } from "../service/callback/vendor/slack";
 import { IIngress } from "../service/ingress";
+import { ISnapshot } from "../service/snapshot";
 import { IVoucher } from "../service/voucher";
 
 import type { DecoratedHandler } from "./base";
@@ -10,6 +11,7 @@ import type { DecoratedHandler } from "./base";
 export type Dependency = {
 	ingress: IIngress;
 	voucher: IVoucher;
+	snapshot: ISnapshot;
 	callback: {
 		vendor: {
 			slack: ICallbackVendorSlack | undefined;
@@ -19,6 +21,7 @@ export type Dependency = {
 const dependency: Dependency = {
 	ingress: container.resolve(IIngress),
 	voucher: container.resolve(IVoucher),
+	snapshot: container.resolve(ISnapshot),
 	callback: {
 		vendor: {
 			slack: container.resolve(ICallbackVendorSlack, true),
@@ -43,12 +46,17 @@ export const primeRoutes = (...args: Handler[]): DecoratedRoutes => {
 	for (const handler of args) {
 		const primed = handler(dependency);
 
+		routers.push(primed.router);
+
+		// TODO: sink endpoints are unsupported for now
+		if (typeof primed.for === "undefined") {
+			continue;
+		}
+
 		handlers[primed.for.path] = {
 			...handlers[primed.for.path],
 			[primed.for.method]: primed.for.handler,
 		};
-
-		routers.push(primed.router);
 	}
 
 	return { routers, handlers };

project/server/src/api/endpoint/callback/vendor/slack/slash-command/handler.test.ts
@@ -19,7 +19,7 @@ test("genuine", async (t: TestContext) => {
 			},
 		});
 
-		const response = await primed.for.handler(
+		const response = await primed.for?.handler(
 			{
 				header: {
 					"x-slack-request-timestamp": 0,
@@ -64,7 +64,7 @@ test("genuine", async (t: TestContext) => {
 
 		t.mock.timers.enable({ apis: ["Date"], now: timestamp * 1000 + 10 * 1000 });
 
-		const response = await primed.for.handler(
+		const response = await primed.for?.handler(
 			{
 				header: {
 					"x-slack-request-timestamp": timestamp,
@@ -102,7 +102,7 @@ test("genuine", async (t: TestContext) => {
 
 		t.mock.timers.enable({ apis: ["Date"], now: timestamp * 1000 });
 
-		const response = await primed.for.handler(
+		const response = await primed.for?.handler(
 			{
 				header: {
 					"x-slack-request-timestamp": timestamp,
@@ -140,7 +140,7 @@ test("genuine", async (t: TestContext) => {
 
 		t.mock.timers.enable({ apis: ["Date"], now: timestamp * 1000 });
 
-		const response = await primed.for.handler(
+		const response = await primed.for?.handler(
 			{
 				header: {
 					"x-slack-request-timestamp": timestamp,

project/server/src/api/endpoint/snapshot/handler.ts
@@ -1,0 +1,240 @@
+import { pipeline, Readable, type TransformCallback } from "node:stream";
+
+import { addHours } from "date-fns";
+import { Schema } from "effect";
+import { isLeft } from "effect/Either";
+import { ArrayFormatter } from "effect/ParseResult";
+import Parser from "stream-json/Parser";
+import StreamBase from "stream-json/streamers/StreamBase";
+import type { ReadonlyDeep } from "type-fest";
+
+import { logger as parentLogger } from "../../../logger";
+import {
+	SnapshotAttachableDevice,
+	SnapshotAttachableEntity,
+	type SnapshotVoucher,
+} from "../../../service/snapshot";
+import { Voucher } from "../../../service/voucher";
+import { isNone, isSome } from "../../../type/maybe";
+import { effectfulSinkEndpoint } from "../../base";
+
+import type { components } from "../../../schema";
+import type { Dependency } from "../../dependency";
+
+const logger = parentLogger.child({ label: "snapshot-v1" });
+
+// https://github.com/uhop/stream-json/issues/174
+class StreamTransform extends StreamBase {
+	// biome-ignore lint/correctness/noUnusedPrivateClassMembers: used by `StreamBase` internals
+	private _level: number;
+	// biome-ignore lint/suspicious/noExplicitAny: also unconstrained in library
+	private _lastKey: any;
+
+	static make(options?: StreamBase.StreamOptions) {
+		return new StreamTransform(options);
+	}
+
+	constructor(options?: StreamBase.StreamOptions) {
+		super(options);
+		// used by `StreamBase` internals
+		this._level = 2;
+		this._lastKey = null;
+	}
+
+	// biome-ignore lint/suspicious/noExplicitAny: also unconstrained in library
+	_wait(chunk: any, _: BufferEncoding, callback: TransformCallback) {
+		if (chunk.name !== "startObject") {
+			return callback(new Error("top-level object should be an object."));
+		}
+		this._transform = this._filter;
+		return this._transform(chunk, _, callback);
+	}
+
+	_push(discard: boolean) {
+		if (this._lastKey === null) {
+			this._lastKey = this._assembler.key;
+		} else {
+			if (!discard) {
+				this.push([
+					// integration
+					this._assembler.path[0],
+					{
+						key: this._lastKey,
+						value: this._assembler.current[this._lastKey],
+					},
+				]);
+			}
+			this._assembler.current = {};
+			this._lastKey = null;
+		}
+	}
+}
+
+const Parameters = Schema.Struct({
+	header: Schema.Struct({
+		"user-agent": Schema.String.pipe(Schema.pattern(/^home-assistant:.+/)),
+		"x-device-database-submission-identifier": Schema.optional(Schema.String),
+	}),
+});
+
+const validatePart = Schema.is(
+	Schema.Tuple(
+		Schema.String,
+		Schema.Struct({
+			key: Schema.Union(Schema.Literal("devices"), Schema.Literal("entities")),
+			value: Schema.Array(Schema.Unknown),
+		}),
+	),
+);
+
+const Device = Schema.Struct({
+	...SnapshotAttachableDevice.fields,
+	entities: Schema.Array(SnapshotAttachableEntity),
+});
+const validateDevice = Schema.is(Device);
+const decodeDevice = Schema.decodeUnknownEither(Device);
+{
+	type Actual = typeof Device.Type;
+	type Wanted = ReadonlyDeep<components["schemas"]["SnapshotV1Device"]>;
+
+	type Assert = Actual extends Wanted ? "yes" : "no";
+	const _: Assert = "yes";
+}
+
+const Entity = SnapshotAttachableEntity;
+const validateEntity = Schema.is(Entity);
+const decodeEntity = Schema.decodeUnknownEither(Entity);
+{
+	type Actual = typeof Entity.Type;
+	type Wanted = components["schemas"]["SnapshotV1Entity"];
+
+	type Assert = Actual extends Wanted ? "yes" : "no";
+	const _: Assert = "yes";
+}
+
+export const postSnapshot1 = (d: Pick<Dependency, "snapshot">) =>
+	effectfulSinkEndpoint(
+		"/api/v1/snapshot/1",
+		"post",
+		Parameters,
+		"application/json",
+		async (parameters, requestBody) => {
+			const submissionIdentifier =
+				parameters.header["x-device-database-submission-identifier"];
+			const hassVersion = parameters.header["user-agent"].replace(
+				"home-assistant:",
+				"",
+			);
+
+			let voucher: SnapshotVoucher;
+			if (typeof submissionIdentifier !== "undefined") {
+				const deserialized =
+					d.snapshot.voucher.deserialize(submissionIdentifier);
+
+				if (deserialized.kind === "success") {
+					voucher = deserialized.voucher;
+				} else {
+					return {
+						code: 400,
+						body: {
+							kind: "malformed-submission",
+							message: "malformed submission",
+						},
+					} as const;
+				}
+			} else {
+				voucher = d.snapshot.voucher.create(new Date());
+			}
+
+			const { id, sub } = Voucher.peek(voucher);
+
+			const handle = await d.snapshot.create(voucher, hassVersion);
+
+			if (isNone(handle)) {
+				// TODO: agree on error
+				return { code: 500, body: "already submitted" } as const;
+			}
+
+			const { promise, resolve, reject } = Promise.withResolvers<void>();
+			const chained = pipeline(
+				Readable.fromWeb(requestBody),
+				new Parser(),
+				StreamTransform.make(),
+				(err) => {
+					// typing doesn't capture it, but `undefined` also indicates successful
+					// pipeline completion
+					if (isSome(err) && typeof err !== "undefined") {
+						reject(err);
+					} else {
+						resolve();
+					}
+				},
+			);
+
+			for await (const part of chained) {
+				if (!validatePart(part)) {
+					continue;
+				}
+
+				const [integration, entry] = part;
+				switch (entry.key) {
+					case "devices": {
+						for (const device of entry.value) {
+							// validation is cheaper than decoding → try validation first, and
+							// only decode in case of an error
+							if (validateDevice(device)) {
+								await d.snapshot.attach.device(
+									handle,
+									integration,
+									device,
+									device.entities,
+								);
+							} else {
+								const decoded = decodeDevice(device);
+								if (isLeft(decoded)) {
+									logger.warn(`submission <${id}> → malformed device`, {
+										submissionId: id,
+										subject: sub,
+										error: ArrayFormatter.formatErrorSync(decoded.left),
+									});
+								}
+							}
+						}
+						break;
+					}
+					case "entities": {
+						for (const entity of entry.value) {
+							// validation is cheaper than decoding → try validation first, and
+							// only decode in case of an error
+							if (validateEntity(entity)) {
+								await d.snapshot.attach.entity(handle, integration, entity);
+							} else {
+								const decoded = decodeEntity(entity);
+								if (isLeft(decoded)) {
+									logger.warn(`submission <${id}> → malformed entity`, {
+										submissionId: id,
+										subject: sub,
+										error: ArrayFormatter.formatErrorSync(decoded.left),
+									});
+								}
+							}
+						}
+						break;
+					}
+				}
+			}
+
+			await promise;
+
+			await d.snapshot.finalize(handle);
+
+			return {
+				code: 200,
+				body: {
+					submission_identifier: d.snapshot.voucher.serialize(
+						d.snapshot.voucher.create(addHours(new Date(), 22), sub),
+					),
+				},
+			} as const;
+		},
+	);

commit bbcb70b164fcf1ac8916965514a14b2085ef416c

fix: request validation
broke due to changes in 1b9602f935b833e083a52a74cde1f2362ef7c81c

project/server/src/api/base.ts
@@ -195,7 +195,7 @@ export const idempotentEndpoint = <
 			requestStorage.getStore() ?? new RequestStorage(),
 			async () => {
 				// biome-ignore lint/suspicious/noExplicitAny: types are checked above
-				return await handler(decodedParameters.right as any);
+				return await handler(decodedParameters as any);
 			},
 		);
 
@@ -384,9 +384,9 @@ export const effectfulEndpoint = <
 			async () => {
 				return await handler(
 					// biome-ignore lint/suspicious/noExplicitAny: types are checked above
-					decodedParameters.right as any,
+					decodedParameters as any,
 					// biome-ignore lint/suspicious/noExplicitAny: types are checked above
-					decodedRequestBody.right as any,
+					decodedRequestBody as any,
 					context,
 				);
 			},
@@ -512,7 +512,7 @@ export const effectfulSinkEndpoint = <
 			async () => {
 				return await handler(
 					// biome-ignore lint/suspicious/noExplicitAny: types are checked above
-					decodedParameters.right as any,
+					decodedParameters as any,
 					requestBody,
 				);
 			},