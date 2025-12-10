9a1c78c5628b9c6278a1e1df9fed96700a018958
Philip Trauner <philip.trauner@arztpraxis.io>
2025-12-10 17:48:00 +0100

    chore: export token for `IVoucher`
    allows forging vouchers during development

--- project/server/src/repl.ts
+++ project/server/src/repl.ts
@@ -1,5 +1,6 @@
 import { container } from "./dependency";
 import { logger } from "./logger";
+import { IVoucher } from "./service/voucher";
 import { unroll } from "./utility/iterable";
 
 logger.level = "debug";
@@ -8,5 +9,5 @@ logger.level = "debug";
 (global as any).container = container;
 (global as any).unroll = unroll;
 
-(global as any).tokens = {};
+(global as any).tokens = { voucher: IVoucher };
 // biome-ignore-end lint/suspicious/noExplicitAny: ↑
