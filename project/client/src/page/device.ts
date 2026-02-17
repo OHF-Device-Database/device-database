35ad9e2de06bc2b11b56f2cafe18a3a5594cf572
Philip Trauner <philip.trauner@arztpraxis.io>
2026-02-17 18:24:41 +0100

    explore: device page

---
+++ project/client/src/page/device.ts
@@ -0,0 +1,72 @@
+import { Schema } from "effect";
+import { LitElement, css, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+import { idempotentOperation } from "../api/base";
+
+import "../element/sized-image";
+
+import { MixinIsomorph } from "../mixin/isomorph";
+
+@customElement("element-page-device")
+export class PageDevice extends MixinIsomorph(LitElement) {
+	@property({ type: String, attribute: "device-id" })
+	deviceId?: string;
+
+	private _deviceTask = (() => {
+		return this.task($X_SYN_LOCATION_TOKEN, {
+			taskFn: async ([id], context) => {
+				if (typeof id === "undefined") {
+					return undefined;
+				}
+
+				const operation = idempotentOperation(
+					"getDevice",
+					"/api/unstable/devices/{id}",
+					"get",
+					{
+						path: { id },
+					}
+				);
+
+				const expected = Schema.Union(
+					Schema.Struct({
+						code: Schema.Literal(200),
+						body: Schema.Struct({
+							integration: Schema.String,
+							manufacturer: Schema.optional(Schema.String),
+							model: Schema.optional(Schema.String),
+							model_id: Schema.optional(Schema.String),
+						}),
+					}),
+					Schema.Struct({
+						code: Schema.Literal(404),
+						body: Schema.Literal("not found"),
+					})
+				);
+
+				return await context.fetch(operation, expected);
+			},
+			argsFn: () => [this.deviceId],
+		});
+	})();
+
+	static styles = css``;
+
+	render() {
+		return html`<main>
+			<pre>id: ${this.deviceId}</pre>
+
+			${this._deviceTask.render({
+				pending: () => html`<p>...</p>`,
+				complete: (response) =>
+					typeof response !== "undefined"
+						? response.code === 200
+							? html`<pre>${JSON.stringify(response.body)}</pre>`
+							: html`<p>${response.body}</p>`
+						: html`<p>no device identifier provided</p>`,
+				error: (e) => html`<p>error: ${e}</p>`,
+			})}
+		</main>`;
+	}
+}
