1e846acfdffdc9709d5e531869191b76baf31acb
Philip Trauner <philip.trauner@arztpraxis.io>
2026-03-10 15:27:27 +0100

    feat!: remove stats endpoint

--- project/server/src/api/endpoint/stats/index.ts
+++
@@ -1,5 +0,0 @@
-import { primeRoutes } from "../../dependency";
-import { getStatsStagingSnapshot } from "./staging/snapshot/handler";
-
-const primed = primeRoutes(getStatsStagingSnapshot);
-export default primed;
