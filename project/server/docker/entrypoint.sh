5f690ebcbfdf825fa809e49ba8cd19527a09d775
Philip Trauner <philip.trauner@arztpraxis.io>
2026-03-16 16:55:01 +0100

    chore: remove unused flag

--- project/server/docker/entrypoint.sh
+++ project/server/docker/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 export NODE_OPTIONS="$(node docker/node-options.ts)"
 
-node --enable-source-maps --experimental-vm-modules out/server/main.mjs
+node --enable-source-maps out/server/main.mjs
