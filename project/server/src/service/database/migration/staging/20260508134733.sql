caa418607dd3548e829a5b44418c1e8871a5583b
Philip Trauner <philip.trauner@arztpraxis.io>
2026-05-22 15:09:28 +0200

    perf: "not in" instead of "in" / recreate some indices
    benchmarks revealed that "in" with a left join is faster than "not in" by about ~2x
    rebalancing indices is sometimes _really_ slow, drop and recreate can be faster

--- project/server/src/service/database/migration/staging/20260508134733.sql
+++ project/server/src/service/database/migration/staging/20260508134733.sql
@@ -24,9 +24,6 @@ create table snapshot_submission_attribution_submission (
 	hass_version text not null,
 	created_at integer not null
 );
-create index snapshot_submission_attribution_submission_snapshot_submission_id_id_idx on snapshot_submission_attribution_submission(snapshot_submission_id, id);
-create index snapshot_submission_attribution_submission_subject_idx on snapshot_submission_attribution_submission(subject);
-create index snapshot_submission_attribution_submission_hass_version_idx on snapshot_submission_attribution_submission(hass_version);
 
 insert into snapshot_submission_attribution_submission
 select
@@ -46,52 +43,96 @@ select
 from
     snapshot_submission ss;
 
-pragma foreign_keys=off;
+create index snapshot_submission_attribution_submission_snapshot_submission_id_id_idx on snapshot_submission_attribution_submission(snapshot_submission_id, id);
+create index snapshot_submission_attribution_submission_subject_idx on snapshot_submission_attribution_submission(subject);
+create index snapshot_submission_attribution_submission_hass_version_idx on snapshot_submission_attribution_submission(hass_version);
 
--- "on cascade" is pretty slow, bulk delete delete attributions referencing soon-to-be deleted submissions
-delete from snapshot_submission_attribution_device where snapshot_submission_id not in (
-    select
-        id
-    from
-        temp_snapshot_submission
-    );
+select '[*] "snapshot_submission_attribution_submission" populated';
 
-delete from snapshot_submission_attribution_device_permutation where snapshot_submission_id not in (
-    select
-        id
-    from
-        temp_snapshot_submission
-    );
+-- "on cascade" is slow, bulk delete delete attributions referencing soon-to-be deleted submissions
+-- "not in" is also slow, "in" with left join is _way_ faster
 
-delete from snapshot_submission_attribution_device_permutation_link where snapshot_submission_id not in (
+delete from snapshot_submission_attribution_device where snapshot_submission_id in (
     select
-        id
+        ss.id
     from
-        temp_snapshot_submission
-    );
+        snapshot_submission ss left join temp_snapshot_submission tss on (
+            tss.id = ss.id
+        )
+    where
+        tss.id is null
+);
 
-delete from snapshot_submission_attribution_set_entity_device_permutation where snapshot_submission_id not in (
+select '[*] orphaned "snapshot_submission_attribution_device" entries cleared';
+
+delete from snapshot_submission_attribution_device_permutation where snapshot_submission_id in (
     select
-        id
+        ss.id
     from
-        temp_snapshot_submission
-    );
+        snapshot_submission ss left join temp_snapshot_submission tss on (
+            tss.id = ss.id
+        )
+    where
+        tss.id is null
+);
+
+select '[*] orphaned "snapshot_submission_attribution_device_permutation" entries cleared';
+
+delete from snapshot_submission_attribution_device_permutation_link where snapshot_submission_id in (
+    select
+        ss.id
+    from
+        snapshot_submission ss left join temp_snapshot_submission tss on (
+            tss.id = ss.id
+        )
+    where
+        tss.id is null
+);
+
+select '[*] orphaned "snapshot_submission_attribution_device_permutation_link" entries cleared';
+
+-- by far the slowest, temporarily drop index to speed up
+drop index snapshot_submission_attribution_set_entity_device_permutation_snapshot_submission_id_idx;
+delete from snapshot_submission_attribution_set_entity_device_permutation where snapshot_submission_id in (
+    select
+        ss.id
+    from
+        snapshot_submission ss left join temp_snapshot_submission tss on (
+            tss.id = ss.id
+        )
+    where
+        tss.id is null
+);
+create index snapshot_submission_attribution_set_entity_device_permutation_snapshot_submission_id_idx on snapshot_submission_attribution_set_entity_device_permutation (snapshot_submission_id);
+
+select '[*] orphaned "snapshot_submission_attribution_set_entity_device_permutation" entries cleared';
 
 -- also delete orphaned entity ←→ device permutation associations
-delete from snapshot_submission_set_entity_device_permutation where id not in (
+delete from snapshot_submission_set_entity_device_permutation where id in (
     select
-        snapshot_submission_set_entity_device_permutation_id
+        sssedp.id
     from
-        snapshot_submission_attribution_set_entity_device_permutation
-);
-delete from snapshot_submission_set_content_entity_device_permutation where snapshot_submission_set_entity_device_permutation_id not in (
-    select
-        id
-    from
-        snapshot_submission_set_entity_device_permutation
+        snapshot_submission_set_entity_device_permutation sssedp join snapshot_submission_attribution_set_entity_device_permutation ssasedp on (
+            sssedp.id = ssasedp.snapshot_submission_set_entity_device_permutation_id
+        )
+    where
+        ssasedp.snapshot_submission_set_entity_device_permutation_id is null
 );
 
-pragma foreign_keys=on;
+select '[*] orphaned "snapshot_submission_set_entity_device_permutation" entries cleared';
+
+delete from snapshot_submission_set_content_entity_device_permutation where snapshot_submission_set_entity_device_permutation_id in (
+    select
+        ssscedp.snapshot_submission_set_entity_device_permutation_id
+    from
+        snapshot_submission_set_content_entity_device_permutation ssscedp left join snapshot_submission_set_entity_device_permutation sssedp on (
+            ssscedp.snapshot_submission_set_entity_device_permutation_id = sssedp.id
+        )
+    where
+        sssedp.id is null
+);
+
+select '[*] orphaned "snapshot_submission_set_content_entity_device_permutation" entries cleared';
 
 delete from snapshot_submission where id not in (
     select
@@ -99,6 +140,8 @@ delete from snapshot_submission where id not in (
     from
         temp_snapshot_submission
     );
+
+select '[*] deduplicated "snapshot_submission"';
 
 alter table snapshot_submission drop column subject;
 alter table snapshot_submission drop column hass_version;
