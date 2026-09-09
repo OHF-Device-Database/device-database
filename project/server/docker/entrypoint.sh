8f7e623181a7c940babd6d764061b50f391757ab
Philip Trauner <philip.trauner@arztpraxis.io>
2026-09-09 14:36:57 +0200

    refactor: entrypoint selection

--- project/server/docker/entrypoint.sh
+++ project/server/docker/entrypoint.sh
@@ -4,4 +4,14 @@ set -e
 
 export NODE_OPTIONS="${NODE_OPTIONS:-$(node docker/node-options.ts)}"
 
-node --enable-source-maps out/server/main.mjs
+case "${NODE_ENTRYPOINT}" in
+    "nest")
+        node --enable-source-maps out/server/main-nest.mjs
+        ;;
+    "default" | "")
+        node --enable-source-maps out/server/main.mjs
+        ;;
+    *)
+        echo "unknown entrypoint \"${NODE_ENTRYPOINT}\""
+        ;;
+esac
