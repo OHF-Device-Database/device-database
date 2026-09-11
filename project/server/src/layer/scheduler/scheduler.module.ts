46fdba52daaecceb47c9ff73be74e90f8fb14a48
Philip Trauner <philip.trauner@arztpraxis.io>
2026-09-11 10:58:21 +0200

    fix: missing subject schedulable
    was not reintroduced in fe4e3534d6a00d300578662499733c1eefa5bcb3, though needed for device
    schedulable

--- project/server/src/layer/scheduler/scheduler.module.ts
+++ project/server/src/layer/scheduler/scheduler.module.ts
@@ -3,20 +3,29 @@ import { Module } from "@nestjs/common";
 import { ModuleIntrospection } from "../introspection/introspection.module";
 import { ModuleSchedulerScheduledDeriveDevice } from "./scheduled/derive/device.module";
 import { ServiceSchedulerScheduledDeriveDevice } from "./scheduled/derive/device.service";
+import { ModuleSchedulerScheduledDeriveSubject } from "./scheduled/derive/subject.module";
+import { ServiceSchedulerScheduledDeriveSubject } from "./scheduled/derive/subject.service";
 import { SchedulerScheduled } from "./scheduler.registry";
 import { ServiceScheduler } from "./scheduler.service";
 
 import type { SchedulerScheduledInstance } from "../../service/scheduler/base";
 
 @Module({
-	imports: [ModuleIntrospection, ModuleSchedulerScheduledDeriveDevice],
+	imports: [
+		ModuleIntrospection,
+		ModuleSchedulerScheduledDeriveDevice,
+		ModuleSchedulerScheduledDeriveSubject,
+	],
 	providers: [
 		{
 			provide: SchedulerScheduled,
 			useFactory: (
 				...units: SchedulerScheduledInstance[]
 			): SchedulerScheduled => units,
-			inject: [ServiceSchedulerScheduledDeriveDevice],
+			inject: [
+				ServiceSchedulerScheduledDeriveDevice,
+				ServiceSchedulerScheduledDeriveSubject,
+			],
 		},
 		ServiceScheduler,
 	],
